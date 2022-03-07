05020d3ffaf724c67178b0b9af244214f549c9d9
sandyr <sandyr@ebi.ac.uk>
2022-03-07 13:27:55 +0000

    adds CLI argument for enabling/disabling cobs cache for index creation

--- src/index.py
+++ src/index.py
@@ -12,6 +12,7 @@ def create(
     index_output: hug.types.text,
     fasta_glob_filter: hug.types.text = "**/*.fna",
     clobber: hug.types.boolean = False,
+    use_cobs_cache: hug.types.boolean = False,
 ):
     """
     Builds a COBS index for a folder (catalogue) of genomes.
@@ -20,11 +21,15 @@ def create(
     :param index_output: Path to / name of the resulting index. .cobs_compact will be auto-appended.
     :param fasta_glob_filter: Glob filter string to find fasta files within `genomes_dir`. E.g. **.fna
     :param clobber: Set True to enable COBS to clobber/overwrite existing outputs.
+    :param use_cobs_cache: Set True to use COBS cache. Indexing will be faster, but pollute genomes_dir with new files.
 
     Catalogues will be built as catalogue_name.cobs_compact/
     """
-    fasta_list = cobs.DocumentList()
+    if not use_cobs_cache:
+        logging.warning("Disabling COBS cache")
+        cobs.disable_cache(True)
 
+    fasta_list = cobs.DocumentList()
     if not Path(genomes_dir).is_dir():
         raise Exception(f"{genomes_dir} does not appear to be a directory")
 
